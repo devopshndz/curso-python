59d47dfd9a34c555b1ca5ad19b613d836e65d1a2
Alberto Hernandez <alberto.hernandez@fracttal.com>
2024-03-12 12:41:55 -0500

    add new lessons 8 y 9

---
+++ 2-tips/9-conversion-tipos.py
@@ -0,0 +1,11 @@
+# conversion de tipos
+# int() -> convierte a entero
+# str() -> convierte a string
+# float() -> convierte a float
+# bool() -> convierte a boolean, tener mucho cuidado con esta función (0, "", None) van a dar flase, el resto dará true
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
