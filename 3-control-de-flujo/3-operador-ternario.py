30edab300039803526c26cf6af72950606e0c83e
Alberto Hernandez <alberto.hernandez@fracttal.com>
2024-03-13 14:59:45 -0500

    update lessons

---
+++ 3-control-de-flujo/3-operador-ternario.py
@@ -0,0 +1,11 @@
+### if de forma mas conciza 
+
+edad = 15
+
+# if edad > 17:
+#     mensaje = "Es mayor"
+# else:
+#     mensaje = "es menor"
+
+mensaje = "Es mayor" if edad > 17 else "es menor" # uso del operador ternario
+print(mensaje)
