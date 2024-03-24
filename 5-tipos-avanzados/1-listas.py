cd4e350b85c40b18d56756e431010fabc4ca2a24
Alberto Hernandez <devopsahernndez@gmail.com>
2024-03-23 23:42:56 -0500

    update lessons

---
+++ 5-tipos-avanzados/1-listas.py
@@ -0,0 +1,21 @@
+### Listas: se utilizan para colocar objetos en un order determinado.
+# se pueden colocar numeros, caracteres, strings, otras listas, poder juntarlas, etc.
+
+numeros = [1, 2, 3] # esto es una lista de numeros. cada numero tiene una posicion [indice] se separan con , cada elemento
+print(numeros) # imprimir la lista
+letras = ["a", "b", "c"]
+palabras = ["Chanchito", "Feliz"]
+palabrasFelices = ["Chanchito", "Feliz", "Alberto"]
+booleand = [True, False, True, False]
+matriz = [[0, 1], [1, 0]] # lista dentro de lista, matriz
+ceros = [0] * 10 # crear lista desde una operación matematica
+aflanumerico = numeros + letras # juntamos dos listas en una, dos listas en una gran lista.
+rango = list(range(1, 11))
+
+print(letras)
+print(palabras)
+print(booleand)
+print(matriz)
+print(ceros)
+print(aflanumerico)
+print(rango)
